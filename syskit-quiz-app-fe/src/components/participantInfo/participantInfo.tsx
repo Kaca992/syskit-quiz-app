63dd3f81e979348c9232a77ee178129655c9b746
Dino Kacavenda <kacavendadino@gmail.com>
2018-05-17 20:43:28 +0200

    downgrade of react because of qr and added 2 more input fields

--- syskit-quiz-app-fe/src/components/participantInfo/participantInfo.tsx
+++ syskit-quiz-app-fe/src/components/participantInfo/participantInfo.tsx
@@ -15,7 +15,7 @@ export interface IParticipantInfoProps {
 
 export default class ParticipantInfo extends React.PureComponent<IParticipantInfoProps> {
     public render() {
-        const { name, email } = this.props.participantInfo;
+        const { name, email, course, enrollmentYear } = this.props.participantInfo;
         const hasValueClassName = "has-value";
 
         return (
@@ -40,6 +40,29 @@ export default class ParticipantInfo extends React.PureComponent<IParticipantInf
                         value={email}
                         underlined
                     />
+                    <div className="course-container">
+                        <div className="course-input">
+                            <TextField
+                                className={course ? hasValueClassName : null}
+                                label={ParticipantInfoInput.courseLabel}
+                                placeholder={ParticipantInfoInput.courseLabel}
+                                onChanged={this._onCourseChanged}
+                                value={course}
+                                underlined
+                            />
+                        </div>
+                        <div className="enrollment-year-input">
+                            <TextField
+                                className={enrollmentYear ? hasValueClassName : null}
+                                label={ParticipantInfoInput.enrollmentYearLabel}
+                                placeholder={ParticipantInfoInput.enrollmentYearLabel}
+                                onChanged={this._onEnrollYearChanged}
+                                value={enrollmentYear}
+                                underlined
+                                type="number"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -47,13 +70,25 @@ export default class ParticipantInfo extends React.PureComponent<IParticipantInf
 
     @autobind
     private _onNameChanged(name: string) {
-        const {onParticipantInfoChanged, participantInfo} = this.props;
-        onParticipantInfoChanged({...participantInfo, name});
+        const { onParticipantInfoChanged, participantInfo } = this.props;
+        onParticipantInfoChanged({ ...participantInfo, name });
     }
 
     @autobind
     private _onEmailChanged(email: string) {
-        const {onParticipantInfoChanged, participantInfo} = this.props;
-        onParticipantInfoChanged({...participantInfo, email});
+        const { onParticipantInfoChanged, participantInfo } = this.props;
+        onParticipantInfoChanged({ ...participantInfo, email });
+    }
+
+    @autobind
+    private _onCourseChanged(course: string) {
+        const { onParticipantInfoChanged, participantInfo } = this.props;
+        onParticipantInfoChanged({ ...participantInfo, course });
+    }
+
+    @autobind
+    private _onEnrollYearChanged(enrollmentYear: string) {
+        const { onParticipantInfoChanged, participantInfo } = this.props;
+        onParticipantInfoChanged({ ...participantInfo, enrollmentYear });
     }
 }
