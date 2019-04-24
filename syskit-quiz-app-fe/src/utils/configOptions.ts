928731c0f16cf371260469f28c549bdbb1fc54cd
Dino Kacavenda <dino.kacavenda@acceleratio.net>
2019-04-24 14:31:00 +0200

    definiranje service endpointa promijenjeno

--- syskit-quiz-app-fe/src/utils/configOptions.ts
+++ syskit-quiz-app-fe/src/utils/configOptions.ts
@@ -1,5 +1,7 @@
+import {serviceEndpoint} from '../assets/config.json';
+
 export function appendServiceApiEndpoint(url: string): string {
     // TODO process env is not working currently, when upgrade to new webpack it will be ok
     // return process.env.NODE_ENV === "production" ? `https://dinotestfunctions.azurewebsites.net/${url}` : `http://localhost:7071/${url}`;
-    return `https://dinotestfunctions.azurewebsites.net/${url}`;
+    return `${serviceEndpoint}${url}`;
 }
